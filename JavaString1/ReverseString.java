6b1c0fc6c24eac0512a2f78b1ba1880ff797cd6c
Amit Kumar <amitkumar4102004b@gmail.com>
2025-09-23 14:45:27 +0530

    added 2nd question

---
+++ JavaString1/ReverseString.java
@@ -0,0 +1,16 @@
+package JavaString1;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String str = "Java";
+        System.out.println("Reversed: " + reverse(str));
+    }
+
+    public static String reverse(String str) {
+        String result = "";
+        for (int i = str.length() - 1; i >= 0; i--)
+            result += str.charAt(i);
+        return result;
+    }
+}
+
