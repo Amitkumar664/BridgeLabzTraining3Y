de3fa2c563d9eb94c03094f02266c9c68b92fee6
Amit Kumar <amitkumar4102004b@gmail.com>
2025-09-23 14:48:52 +0530

    added 7th question

---
+++ JavaString1/ToggleCase.java
@@ -0,0 +1,19 @@
+package JavaString1;
+
+public class ToggleCase {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        System.out.println(toggleCase(str));
+    }
+
+    public static String toggleCase(String str) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : str.toCharArray()) {
+            if (Character.isUpperCase(c)) sb.append(Character.toLowerCase(c));
+            else if (Character.isLowerCase(c)) sb.append(Character.toUpperCase(c));
+            else sb.append(c);
+        }
+        return sb.toString();
+    }
+}
+
